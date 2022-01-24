3cd8b8889507b024d0297121a50bed4aab27fc7c
Kilian LE DU <ledu.kilian@gmail.com>
2022-01-24 16:32:09 +0100

    Added constraint and labels to form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,42 +5,76 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
+                'attr' => ['class' => 'w-100'],
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un nom d\'utilisateur valide',
+                    ]),
+                    new NotNull([
+                        'message' => 'Veuillez saisir un nom d\'utilisateur valide',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre nom d\'utilisateur doit contenir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                        'maxMessage' => 'Votre nom d\'utilisateur ne doit pas contenir plus de {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse mail',
+                'attr' => ['class' => 'w-100'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un email valide',
+                    ]),
+                    new NotNull([
+                        'message' => 'Veuillez saisir un email valide',
+                    ]),
+                    new Email([
+                        'message' => 'Veuillez saisir un format d\'email valide',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre email doit contenir au moins {{ limit }} caractères',
+                        'max' => 4096,
+                        'maxMessage' => 'Votre email ne doit pas contenir plus de {{ limit }} caractères',
                     ]),
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'label' => 'Mot de passe',
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'w-100'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Veuillez entrer un mot de passe valide',
                     ]),
+                    new NotNull([
+                        'message' => 'Veuillez saisir un mot de passe valide',
+                    ]),
                     new Length([
                         'min' => 6,
                         'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                        'maxMessage' => 'Votre mot de passe ne doit pas contenir plus de {{ limit }} caractères',
                     ]),
                 ],
             ])

860148e35954414118c4f3b9d17a06e9903423e0
Kilian LE DU <ledu.kilian@gmail.com>
2022-02-19 06:02:15 +0100

    Updated IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -25,19 +25,7 @@ class IndexController extends AbstractController
             'tricks' => $tricks
         ]);
     }
-    /**
-     * @Route("/trick", name="show_trick")
-     */
-    public function showTrick()
-    {
-        return $this->render('@client/pages/trick.html.twig', []);
-    }
-    /**
-     * @Route("/admin", name="show_admin")
-     */
-    public function showAdmin()
-    {
-        $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('@client/pages/admin.html.twig', []);
-    }
+
+
+
 }

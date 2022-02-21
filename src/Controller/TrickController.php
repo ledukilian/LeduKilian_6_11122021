7e3f49765dd3354a9f08142ed2ccbf774cf58992
Kilian LE DU <ledu.kilian@gmail.com>
2022-02-21 15:41:48 +0100

    Added new trick list page

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -17,46 +17,10 @@ class TrickController extends AbstractController
     /**
      * @Route("/trick/{slug}", name="show_trick")
      */
-    public function showTrick(SerializerInterface $serializer, ManagerRegistry $doctrine, String $slug)
+    public function showTrick(Trick $trick)
     {
-        $trick = $doctrine->getRepository(Trick::class)->findOneBy(['slug' => $slug]);
-
-        if (!$trick) {
-            throw $this->createNotFoundException(
-                'Aucun trick pour le slug '.$slug
-            );
-        }
-
-        $contributions = [];
-        foreach($trick->getContributors() as $contributor) {
-            $contributions[] = [
-                'name' => $contributor->getUser()->getUsername(),
-                'date' => date_format($contributor->getCreatedAt(), 'd/m/Y à H:i')
-            ];
-        }
-
-        $comments = [];
-        foreach($trick->getComments() as $comment) {
-            if ($comment->getStatus()) {
-                $comments[] = [
-                    'author' => $comment->getUser()->getUsername(),
-                    'content' => $comment->getContent(),
-                    'date' => date_format($comment->getCreatedAt(), 'd/m/Y à H:i')
-                ];
-            }
-        }
         return $this->render('@client/pages/trick.html.twig', [
-            'trick' => [
-                'id' => $trick->getId(),
-                'name' => $trick->getName(),
-                'description' => $trick->getDescription(),
-                'slug' => $trick->getSlug(),
-                'category' => $trick->getCategory()->getName(),
-                'author' => $trick->getUser()->getUsername(),
-                'date' => date_format($trick->getCreatedAt(), 'd/m/Y à H:i'),
-                'contributors' => $contributions,
-                'comments' => $comments
-            ],
+            'trick' => $trick,
             'remain_comments' => false
         ]);
     }
@@ -64,10 +28,20 @@ class TrickController extends AbstractController
     /**
      * @Route("/tricks", name="show_tricks")
      */
-    public function showTricks()
+    public function showTricks(ManagerRegistry $doctrine)
     {
-        return $this->render('@client/pages/trick.html.twig', []);
-    }
+        $tricks = $doctrine
+            ->getRepository(Trick::class)
+            ->findBy(
+                [],
+                ['createdAt' => 'DESC'],
+                9,
+                0
+            );
+        return $this->render('@client/pages/tricks.html.twig', [
+            'tricks' => $tricks,
+            'remain_tricks' => true
+        ]);    }
 
 
 

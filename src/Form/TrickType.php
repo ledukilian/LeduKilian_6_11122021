ef2dcc7168aa8be9069efa0a303be7ddb9ac5467
ledukilian <ledu.kilian@gmail.com>
2022-05-21 00:57:21 +0200

    Removed unused import

--- src/Form/TrickType.php
+++ src/Form/TrickType.php
@@ -7,7 +7,6 @@ use App\Entity\Trick;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -73,7 +72,7 @@ class TrickType extends AbstractType
                 'entry_type' => MediaType::class,
                 'entry_options' => ['label' => false],
                 'allow_add' => true,
-                'allow_delete' => true,
+                'allow_delete' => true
             ])
         ;
     }
